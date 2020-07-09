4be06e7128d12ea33383a57dd749807843e892e7
Dhi Aurrahman <dio@tetrate.io>
2020-07-09 15:09:57 +0700

    llvm@9

--- Formula/envoy.rb
+++ Formula/envoy.rb
@@ -5,7 +5,7 @@ class Envoy < Formula
       :tag     => "v1.15.0",
       :revision => "50ef0945fa2c5da4bff7627c3abf41fdd3b7cffd"
 
-  depends_on "llvm@10" => :build
+  depends_on "llvm@9" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "bazelisk" => :build
@@ -15,15 +15,15 @@ class Envoy < Formula
   depends_on "ninja" => :build
 
   def install
-    llvm = Formula["llvm@10"]
-    
+    llvm = Formula["llvm@9"]
+
     ENV.prepend_create_path "BAZEL_LINKOPTS", "-L#{llvm.opt_lib} -Wl,-rpath,#{llvm.opt_lib}"
     ENV.prepend_create_path "BAZEL_CXXOPTS", "-I#{llvm.opt_include}"
-    
+
     action_env = "PATH=#{llvm.opt_bin}:#{HOMEBREW_PREFIX}/bin:/usr/bin:/bin"
 
     target = "source/exe:envoy-static.stripped"
-    system "bazelisk", "build", "--action_env=#{action_env}", "//#{target}"
+    system "bazelisk", "build", "--action_env=#{action_env}", "--verbose_failures", "//#{target}"
     bin.install "bazel-bin/#{target}" => "envoy"
   end
 end
