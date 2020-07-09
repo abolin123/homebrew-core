7c6691e9ab657f673ff913c539faddbadf04ded2
Dhi Aurrahman <dio@tetrate.io>
2020-07-09 14:00:50 +0700

    As is

--- Formula/envoy.rb
+++ Formula/envoy.rb
@@ -5,17 +5,32 @@ class Envoy < Formula
       :tag     => "v1.15.0",
       :version => "50ef0945fa2c5da4bff7627c3abf41fdd3b7cffd"
 
-  depends_on "llvm@10" => :build
+  depends_on "aspell" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "bazelisk" => :build
+  depends_on "clang-format" => :build
   depends_on "cmake" => :build
   depends_on "coreutils" => :build
+  depends_on "go" => :build
   depends_on "libtool" => :build
   depends_on "ninja" => :build
+  depends_on "wget" => :build
 
   def install
-    ENV.prepend_path "PATH", Formula["llvm@10"].opt_bin
-    system "bazelisk", "build", "//source/exe:envoy-static"
+    args = %w[
+      --curses=no
+      --show_task_finish
+      --verbose_failures
+      --action_env=PATH=/usr/local/bin:/opt/local/bin:/usr/bin:/bin
+      --test_output=all
+    ]
+    system "bazelisk", "build", *args, "//source/exe:envoy-static"
+
+    pp Dir["**/*"]
+  end
+
+  test do
+    system bin/"envoy", "--help"
   end
 end
